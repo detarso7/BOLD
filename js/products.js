fb468ada72807df6eb498cc16e5ffabd8b743b46
Saulo de Tarso <de_tarso7@hotmail.com>
2023-04-29 00:37:21 -0300

    Segunda requisição e preparando para finalizar

--- js/products.js
+++ js/products.js
@@ -1,8 +1,11 @@
 // Define a URL da API a ser acessada
-const url = "https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1" 
+const url = "https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=1"
+const url2 = "https://frontend-intern-challenge-api.iurykrieger.now.sh/products?page=2" 
+
 
 // Seleciona o elemento HTML que conterá os cards dos produtos
 const productCard = document.querySelector("#card")
+const productCard2 = document.querySelector("#card-2")
 
 // Define uma função assíncrona que irá acessar a API e exibir as informações dos produtos na tela
 async function getProducts () {
@@ -26,6 +29,7 @@ async function getProducts () {
 		const price = document.createElement("span")
 		const installmentsoldPrice = document.createElement("p")
 		const buttonProduct = document.createElement("a")
+		const br = document.createElement("br")
 
 		// Inseri as informações da requisição
 		img.setAttribute("src", `${pro.image}`)
@@ -43,6 +47,7 @@ async function getProducts () {
 		div.appendChild(oldPrice)
 		div.appendChild(price)
 		div.appendChild(installmentsoldPrice)
+		div.appendChild(br)
 		div.appendChild(buttonProduct)
 
 		// Inseri a dive montada no elemento selecionado anteriormente
@@ -52,3 +57,55 @@ async function getProducts () {
 }
 
 getProducts()
+
+// Seleciona o elemento HTML que conterá os cards dos produtos
+
+async function getMoreProducts () {
+
+	const response = await fetch(url2)
+	const data = await response.json()
+
+	console.log(data)
+
+
+	data.products.map((pro) =>{
+
+		
+		const div = document.createElement("div")
+
+		
+		const img = document.createElement("img")
+		const productName = document.createElement("h6")
+		const text = document.createElement("p")
+		const oldPrice = document.createElement("p")
+		const price = document.createElement("span")
+		const installmentsoldPrice = document.createElement("p")
+		const buttonProduct = document.createElement("a")
+		const br = document.createElement("br")
+
+		
+		img.setAttribute("src", `${pro.image}`)
+		productName.innerText = pro.name
+		text.innerText = pro.description
+		oldPrice.innerText = `De: R$ ${pro.oldPrice},00`
+		price.innerText = `Por R$ ${pro.price},00`
+		installmentsoldPrice.innerText = `Ou ${pro.installments.count}x de: R$ ${pro.installments.value}0`
+		buttonProduct.innerText = "Comprar"
+
+		
+		div.appendChild(img)
+		div.appendChild(productName)
+		div.appendChild(text)
+		div.appendChild(oldPrice)
+		div.appendChild(price)
+		div.appendChild(installmentsoldPrice)
+		div.appendChild(br)
+		div.appendChild(buttonProduct)
+
+		
+		productCard.appendChild(div)
+
+	})
+}
+
+getMoreProducts()
