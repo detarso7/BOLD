d7f71bacd4ac0c2c963621f2f2b9492da8995cb1
Saulo de Tarso <de_tarso7@hotmail.com>
2023-04-30 15:04:01 -0300

    Comentando o código

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,9 @@
-const hidenButton = document.getElementsByClassName('hiden')
+// Seleciona todos os elementos
+const hidenButton = document.getElementsByClassName('hiden') 
 const sectionHiden = document.getElementsByClassName('products-2')
 
+// Cria uma função chamada "onClickHiden"
 function onClickHiden(){
-    sectionHiden[0].style.display = 'flex'
-    hidenButton[0].style.display = 'none'
+    sectionHiden[0].style.display = 'flex' // Define a propriedade "display" do primeiro elemento da variável "sectionHiden" como "flex"
+    hidenButton[0].style.display = 'none' // Define a propriedade "display" do primeiro elemento da variável "hidenButton" como "none"
 }
--- js/validation.js
+++ js/validation.js
@@ -1,18 +1,32 @@
+// Seleção das tags 
+
 const form = document.getElementById('form')
 const inputs = document.querySelectorAll('.requered')
 const spans = document.querySelectorAll('.span-requared')
+
+// Definir a variável "emailREGEX" para uma expressão regular que verifica se o valor 
+//inserido em um input corresponde ao formato de um endereço de email
+
 const emailREGEX = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
-const button = document.getElementById('#btn')
+
+// Definir a função "setError" para aplicar um estilo que indica um erro 
+//ao input com o índice fornecido e mostrar a mensagem de erro correspondente
 
 function setError(index){
     inputs[index].style.border = '2px solid #e63636'
     spans[index].style.display = 'block'
 }
 
+// Definir a função "setSuccess" para remover o estilo de erro do input com o 
+//índice fornecido e ocultar a mensagem de erro correspondente
+
 function setSucess(index){
     inputs[index].style.border = ''
     spans[index].style.display = 'none'
 }
+
+// Definir a função "nameValidate" para verificar se o valor inserido no primeiro 
+//input tem pelo menos 3 caracteres e aplicar o estilo correspondente
 
 function nameValidate () {
     if(inputs[0].value.length < 3) {
@@ -22,6 +36,9 @@ function nameValidate () {
         setSucess(0)
     }
 }
+
+// Definir a função "emailValidate" para verificar se o valor inserido no segundo input
+//corresponde ao formato de um endereço de email e aplicar o estilo correspondente
 
 function emailValidate () {
     if(emailREGEX.test(inputs[1].value)) {
